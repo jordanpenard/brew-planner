7270fd946fe158f7c0761501ed14d1b1843d3caf
Jordan Penard <jordan038@gmail.com>
2023-03-21 16:36:04 +0000

    Fixed beer color estimation

--- stock/models.py
+++ stock/models.py
@@ -51,23 +51,56 @@ class Recipe(models.Model):
     mash_temperature_c = models.FloatField(default=0)
 
     def lovibondToRgb(self, lovibond):
+        # From http://brew-engine.com/engines/beer_color_calculator.html
+
+        srm = (1.3546*lovibond)-0.76
 
         r = 0
         g = 0
         b = 0
 
-        if lovibond < 1.7:
-            r = 226.6017
-            g = 174.7456
-            b = 101.7924
-        elif lovibond > 600:
-            r = 76.52
-            g = 58.344
-            b = 47.936
-        else:
-            r = 227.3 - 0.4112 * lovibond + 0.0002665 * lovibond * lovibond
-            g = 175.5 - 0.4445 * lovibond + 0.0004154 * lovibond * lovibond
-            b = 102.2 - 0.2402 * lovibond + 0.0002496 * lovibond * lovibond
+        if 0 <= srm <= 1:
+            r = 240
+            g = 239
+            b = 181
+        
+        elif 1 < srm <= 2:
+            r = 233
+            g = 215
+            b = 108
+
+        elif srm > 2:
+            # Set red decimal
+            if srm < 70.6843:
+                r = 243.8327-6.4040 * srm+0.0453 * srm * srm
+            else:
+                r = 17.5014
+            
+            # Set green decimal
+            if srm < 35.0674:
+                g = 230.929-12.484 * srm+0.178 * srm * srm
+            else:
+                g = 12.0382
+            
+            # Set blue decimal
+            if srm < 4:
+                b = -54 * srm+216
+            elif 4 <= srm < 7:
+                b = 0
+            elif 7 <= srm < 9:
+                b = 13 * srm-91
+            elif 9 <= srm < 13:
+                b = 2 * srm+8
+            elif 13 <= srm < 17:
+                b = -1.5 * srm+53.5
+            elif 17 <= srm < 22:
+                b = 0.6 * srm+17.8
+            elif 22 <= srm < 27:
+                b = -2.2 * srm+79.4
+            elif 27 <= srm < 34:
+                b = -0.4285 * srm + 31.5714
+            else:
+                b = 17
 
         return "rgb(" + str(r) + ", " + str(g) + ", " + str(b) + ")"
 
