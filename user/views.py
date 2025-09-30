5a3ca488dd879ca654962a61c991f6d3fe3d347b
Jordan Penard <jordan.penard@blaize.com>
2025-09-30 14:38:42 +0200

    Improve login page and add active nav item on nav bar

--- user/views.py
+++ user/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django import forms
 
+
+class Loginform(forms.Form):
+    username=forms.CharField()
+    password=forms.CharField(widget=forms.PasswordInput)
+    remember_me=forms.BooleanField(required=False)
 
 def login_user(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("index")
+        form=Loginform(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            remember_me = form.cleaned_data['remember_me']
+            user = authenticate(request, username=username, password=password)
+    
+            if user is not None:
+                login(request, user)
+                if not remember_me:
+                    request.session.set_expiry(0)  # <-- Here if the remember me is False, that is why expiry is set to 0 seconds. So it will automatically close the session after the browser is closed.
+
+                return redirect("index")
+            else:
+                messages.success(request, "Invalid credentials")
+                return redirect("login")
         else:
-            messages.success(request, "Invalid credentials")
-            return redirect("login")
+            return render(request, 'login.html')
     else:
         return render(request, 'login.html')
